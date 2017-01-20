43e94bfbdeeea6ec5f674c66ca28e88e15234b91
Justin Purnell <jpurnell@swoop.local>
2017-01-20 14:29:07 -0500

    Generalized to accommodate different Flickr endpoints, "Recent Photos" as the proof of concept

--- PhotoStore.swift
+++ PhotoStore.swift
@@ -6,7 +6,16 @@
 //  Copyright © 2017 Justin Purnell. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum ImageResult {
+	case success(UIImage)
+	case failure(Error)
+}
+
+enum PhotoError: Error {
+	case imageCreationError
+}
 
 enum PhotosResult {
 	case success([Photo])
@@ -19,26 +28,70 @@ class PhotoStore {
 		return URLSession(configuration: config)
 	}()
 	
-	func fetchInterestingPhotos() {
-		
+	func fetchInterestingPhotos(completion: @escaping (PhotosResult) -> Void) {
+
 		let url = FlickrAPI.interestingPhotosURL
 		let request = URLRequest(url: url)
 		let task = session.dataTask(with: request) {
 			(data, response, error) -> Void in
-			if let jsonData = data {
-				do {
-					let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: [])
-					print(jsonObject)
-				} catch let error {
-					print("Error creating JSON object: \(error)")
-				}
-			} else if let requestError = error {
-				print("Error fetching interesting photos: \(requestError)")
-			} else {
-				print("Unexpected error with the request")
+			let result = self.processPhotosRequest(data: data, error: error)
+			OperationQueue.main.addOperation {
+				completion(result)
+			}
+
+		}
+		task.resume()
+	}
+	
+	func fetchPhotos(method: Method, completion: @escaping (PhotosResult) -> Void) {
+		let url = FlickrAPI.photosURL(method)
+		let request = URLRequest(url: url)
+		let task = session.dataTask(with: request) {
+			(data, response, error) -> Void in
+			let result = self.processPhotosRequest(data: data, error: error)
+			OperationQueue.main.addOperation {
+				completion(result)
 			}
 		}
 		task.resume()
 	}
 	
+	private func processPhotosRequest(data: Data?, error: Error?) -> PhotosResult {
+		guard let jsonData = data else {
+			return .failure(error!)
+		}
+		return FlickrAPI.photos(fromJSON: jsonData)
+	}
+	
+	func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+		
+		let photoURL = photo.remoteURL
+		let request = URLRequest(url: photoURL)
+		
+		let task = session.dataTask(with: request) {
+			(data, response, error) -> Void in
+			let responseHeaders = response as! HTTPURLResponse
+			print("Response status code: \(responseHeaders.statusCode)")
+			print("Response header fields: \(responseHeaders.allHeaderFields)")
+			let result = self.processImageRequest(data: data, error: error)
+			OperationQueue.main.addOperation {
+				completion(result)
+			}
+		}
+		task.resume()
+	}
+	
+	private func processImageRequest(data: Data?, error: Error?) -> ImageResult {
+		guard
+		let imageData = data,
+			let image = UIImage(data: imageData) else {
+				// Couldn't create an image
+				if data == nil {
+					return .failure(error!)
+				} else {
+					return .failure(PhotoError.imageCreationError)
+				}
+		}
+		return .success(image)
+	}
 }
